1d39976bc94ff0e5369c61adad5ec4676837a662
Farai Major <faraimutukumira@gmail.com>
2022-09-19 23:09:19 -0400

    length of last word

---
+++ src/LengthofLastWord.java
@@ -0,0 +1,14 @@
+public class LengthofLastWord {
+
+    public static void main(String[] args) {
+        String st = "Hello World wadinhai";
+
+        System.out.print(lengthOfLastWord(st));
+    }
+    public static int lengthOfLastWord(String s) {
+        String [] arr = s.split(" ");
+        String str = arr[arr.length -1];
+        return str.length();
+
+    }
+}
