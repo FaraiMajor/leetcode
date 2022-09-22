f52a323273baa68370296dab1d382c152172b518
Farai Major <faraimutukumira@gmail.com>
2022-09-21 23:29:09 -0400

    concatenation of arrays

---
+++ src/ConcatenationOfArray.java
@@ -0,0 +1,13 @@
+public class ConcatenationOfArray {
+    public int[] getConcatenation(int[] nums) {
+
+        int [] ans = new int [2*nums.length];
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
+        }
+        return ans;
+    }
+
+}
