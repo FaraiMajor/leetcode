935cb06409a95a8dec04c10ba31b3ac0d53c4dbf
Farai Major <faraimutukumira@gmail.com>
2022-09-27 23:38:42 -0400

    square root

---
+++ src/Sqrt.java
@@ -0,0 +1,38 @@
+public class Sqrt {
+    public int mySqrt(int x) {
+
+        if(x<0)
+            return -1;
+        if(x==0)
+            return 0;
+        if(x<4)
+            return 1;
+
+        int low = 1;
+        int high = x;
+        while(low < high){
+            int mid = low + (high - low)/2;
+
+            //int sqr = mid * mid;
+            if(x/mid == mid){
+                return mid;
+            }else if(x/mid > mid){
+                low = mid + 1;
+            }else{
+                high = mid;
+            }
+        }
+        return low -1;
+    }
+
+//    second option but slow
+
+//    public int mySqrt(int x) {
+//
+//        long i = 1;
+//        while (x >= i * i) {
+//            i++;
+//        }
+//        return (int) i - 1;
+//    }
+}
